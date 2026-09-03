62eab9bcafc6713630f0b6c8323e2f8020bdb222
priyadharshinig07 <priyadharshinigopinath27@gmail.com>
2026-09-03 15:22:20 +0530

    Fix import path for monitor module

--- drift_monitor/demo_replay.py
+++ drift_monitor/demo_replay.py
@@ -1,5 +1,5 @@
 import json
-from monitor import classify_severity, response_for_severity
+from drift_monitor.monitor import classify_severity, response_for_severity
 
 sequence = json.load(open("testdata/fixtures/drift_sequence.json"))
 
